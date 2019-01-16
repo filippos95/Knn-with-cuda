5fde0250dd21d7a5e6ab7cd15ea648ccf2bcaaf3
Unknown <fkasioulis.95@gmail.com>
2019-01-16 22:10:35 +0200

    Serial #2
    Make the fragmentation points to blocks

--- main.c
+++ main.c
@@ -6,15 +6,24 @@
 #include <time.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 
-struct Point {
+ struct Point {
     float x;
     float y;
     float z;
 
 };
 
+
+typedef struct{
+    float max_dist;
+    float min_dist;
+    struct Point k;
+
+
+}kNeighbour;
 
 void PrintPoints(struct Point *arr,int number ) {
     for (int i = 0; i < number; i++) {
@@ -33,22 +42,53 @@ void generatePoints( struct Point *arr ,int number){
 
 }
 
+float distanceOfPoints( struct Point p1,struct Point p2){
+    float dist=0;
+    dist=pow(p1.x-p2.x,2)+pow(p1.y-p2.y,2)+pow(p1.z-p2.z,2);
+    dist=sqrt(dist);
+    return dist;
+
+
+}
+
+
+void putPointsBlocks(int *pointsToBlock,struct Point *allPoints,int *perBlockPoints,int numberOfPoints,int gridDim,int blockLength) {
+         int blockId=0;
+         for (int i=0; i<numberOfPoints;i++){
+           blockId=floor(allPoints[i].x/blockLength)+floor(allPoints[i].y/blockLength)*gridDim+floor(allPoints[i].z/blockLength)*gridDim*gridDim);
+           pointsToBlock[i]=blockId;
+           perblockpoints[blockId]++;
+
+         }
+}
 
 int main(int argc,char** argv){
 
-int numberOfPoints=atoi(argv[1]);
-numberOfPoints=pow(2,numberOfPoints);
+int numberOfcPoints=atoi(argv[1]);
+numberOfcPoints=pow(2,numberOfcPoints);
 int dimOfGrid=atoi(argv[2]);
 dimOfGrid=pow(2,dimOfGrid);
+int numberOfBlocks=pow(dimOfGrid,3);
+int numberOfqpoints=numberOfcPoints;
+struct Point *cpoints=malloc(numberOfnPoints*sizeof(struct Point));
+struct Point *qpoints=malloc(numberOfqpoints*sizeof(struct Point));
+int *pointsctoblock=malloc(numberOfcPointsPoints*sizeof(int));
+int *pointsqtoblock=malloc(numberOfqpoints*sizeof(int));
+int *perblockcpoints=malloc(numberOfBlocks*sizeof(int));
+int *perblockqpoints=malloc(numberOfBlocks*sizeof(int));
+
+generatePoints(cpoints,numberOfcPoints);
+generatePoints(qpoints,numberOfqpoints);
+//PrintPoints(npoints,numberOfcPoints);
+//PrintPoints(qpoints,numberOfqpoints);
+float block_length= ((float)1)/((float)dimOfGrid);
+
+putPointsBlocks(pointsctoblock,cpoints,perblockcpoints,numberOfcPoints,dimOfGrid,block_length);
+putPointsBlocks(pointsqtoblock,qpoints,perblockqpoints,numberOfqpoints,dimOfGrid,block_length);
 
 
 
-
-struct Point *points=malloc(numberOfPoints*sizeof(struct Point));
-generatePoints(points,numberOfPoints);
-PrintPoints(points,numberOfPoints);
 
 return 0;
-
 
 }
